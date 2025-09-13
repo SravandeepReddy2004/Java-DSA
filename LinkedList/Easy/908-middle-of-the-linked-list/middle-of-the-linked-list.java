4b3014ebc7b44ef19f8fe980b16f8daef2d13e3f
S Sravandeep Reddy <sravanreddy254@gmail.com>
2025-09-13 20:45:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (32.80%) - LeetSync

--- LinkedList/Easy/908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ LinkedList/Easy/908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -10,12 +10,19 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode fast=head;
-        ListNode slow=head;
-        while(fast!=null&&fast.next!=null&&slow!=null){
-            fast=fast.next.next;
-            slow=slow.next;
+        // Initialize two pointers
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null&& slow != null) {
+            fast = fast.next.next;// Move fast by 2 steps and slow by 1 step
+            slow = slow.next;
         }
+        // When fast reaches end, slow will be at the middle
         return slow;
     }
 }
+//fast != null → ensures we haven’t passed the end.
+
+//fast.next != null → ensures fast.next.next won’t cause a NullPointerException.
+
+//slow != null → not strictly needed (because if head is not null, slow will always stay valid). You could remove this and it’d still work.
