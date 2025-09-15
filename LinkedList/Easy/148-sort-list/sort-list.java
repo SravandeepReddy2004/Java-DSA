e0c70eedd0d1a698ce633726fda336df14b6a299
S Sravandeep Reddy <sravanreddy254@gmail.com>
2025-09-15 22:06:31 +0530

    Time: 9 ms (96.31%) | Memory: 57.3 MB (16.55%) - LeetSync

--- LinkedList/Easy/148-sort-list/sort-list.java
+++ LinkedList/Easy/148-sort-list/sort-list.java
@@ -9,19 +9,49 @@
  * }
  */
 class Solution {
+    private ListNode merge(ListNode l1,ListNode l2){
+        ListNode sorted=new ListNode(0);
+        ListNode temp=sorted;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){ //sort
+                temp.next=l1;
+                l1=l1.next;
+            }
+            else{
+                temp.next=l2;
+                l2=l2.next;
+            }
+            temp=temp.next; //traverse
+        }
+        if(l1!=null){
+            temp.next=l1;
+            l1=l1.next;
+        }
+        else{
+            temp.next=l2;
+            l2=l2.next;
+        }
+
+        return sorted.next;
+    }
     public ListNode sortList(ListNode head) {
-        List<Integer> arr=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            arr.add(temp.val);
-            temp=temp.next;
+        ListNode fast=head,slow=head,prev=null;
+        if(head==null||head.next==null){
+            return head;
         }
-        Collections.sort(arr);
-        temp=head;
-        for(int i=0;i<arr.size();i++){
-            temp.val=arr.get(i);
-            temp=temp.next;
+        while(fast!=null && fast.next!=null){// Find the middle of the list
+            prev=slow;
+            fast=fast.next.next;
+            slow=slow.next;
         }
-        return head;
+        // Split the list into two halves
+        prev.next=null;                         //head->prev & slow->fast
+
+        // Recursively sort each half
+        ListNode right=sortList(slow);
+        ListNode left=sortList(head);
+
+        // Merge the sorted halves
+        return merge(right,left);
     }
 }
