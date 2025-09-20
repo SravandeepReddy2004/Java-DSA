86bac53614ff97db9c564d0d2b3d34f05cccbcb2
S Sravandeep Reddy <sravanreddy254@gmail.com>
2025-09-20 22:06:29 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (17.45%) - LeetSync

--- LinkedList/Easy/328-odd-even-linked-list/odd-even-linked-list.java
+++ LinkedList/Easy/328-odd-even-linked-list/odd-even-linked-list.java
@@ -6,7 +6,7 @@ class Solution {
         ListNode even = head.next; //even is always the next of odd
         ListNode evenHead = even; //store link for later joining the 2 lists together
         
-        while(even!=null && even.next!=null){ //as last node will always be even restrict it
+        while(even!=null && even.next!=null){ //as last node will always be even,so restrict it
             odd.next = even.next; //odd next is even
             odd = odd.next; //traverse odd node
             even.next = odd.next; //just do the same in terms of even
