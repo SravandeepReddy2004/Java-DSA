ba70be5036e827f6da1a87a27c3abee34aec0419
S Sravandeep Reddy <sravanreddy254@gmail.com>
2025-09-21 21:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (92.86%) - LeetSync

--- Stack-and-Queues/225-implement-stack-using-queues/implement-stack-using-queues.java
+++ Stack-and-Queues/225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -8,9 +8,9 @@ class MyStack {
 
     public void push(int x) {
         q.add(x);
-        for (int i = 0; i < q.size() - 1; i++) {
-            q.add(q.poll());
-        }
+        
+        for (int i = 0; i < q.size() - 1; i++)
+          q.add(q.poll());
     }
 
     public int pop() {
