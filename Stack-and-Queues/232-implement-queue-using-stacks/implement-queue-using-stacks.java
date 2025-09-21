56f4d379071a2f1d0c26d318083a3c1ca30a9c56
S Sravandeep Reddy <sravanreddy254@gmail.com>
2025-09-21 22:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (19.53%) - LeetSync

--- Stack-and-Queues/232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ Stack-and-Queues/232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,41 +1,41 @@
 class MyQueue { 
-    private Stack<Integer> in;   // main stack (stores queue)
-    private Stack<Integer> out;  // helper stack (used during push)
+    private Stack<Integer> s1;   // main stack (stores queue)
+    private Stack<Integer> s2;  // helper stack (used during push)
 
     // Constructor
     public MyQueue() {
-       in = new Stack<>();
-       out = new Stack<>();  
+       s1 = new Stack<>();
+       s2 = new Stack<>();  
     }
     
     // Push element to the back of queue
     public void push(int x) {
-        // Move all elements from in → out
-        while (!in.isEmpty()) {
-            out.push(in.pop());
+        // Move all elements from s1 → s2
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
         }
 
-        // Add new element to in
-        in.push(x);
+        // Add new element to s1
+        s1.push(x);
 
-        // Move everything back from out → in
-        while (!out.isEmpty()) {
-            in.push(out.pop());
+        // Move everything back from s2 → s1
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
         }
     }
     
     // Remove element from the front
     public int pop() {
-        return in.pop(); // top of in is the front of queue
+        return s1.pop(); // top of s1 is the front of queue
     }
     
     // Get the front element
     public int peek() {
-        return in.peek(); // top of in is the front
+        return s1.peek(); // top of s1 is the front
     }
     
     // Check if queue is empty
     public boolean empty() {
-        return in.isEmpty();
+        return s1.isEmpty();
     }
 }
