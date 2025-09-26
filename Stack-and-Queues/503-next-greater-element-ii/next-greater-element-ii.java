06474ff54a959d89a813cd63f8ef327e4c666a40
S Sravandeep Reddy <sravanreddy254@gmail.com>
2025-09-26 22:31:51 +0530

    Time: 89 ms (10.71%) | Memory: 46.1 MB (74.13%) - LeetSync

--- Stack-and-Queues/503-next-greater-element-ii/next-greater-element-ii.java
+++ Stack-and-Queues/503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n=nums.length;
-        int ans[]=new int[n];
-        for(int i=0;i<n;i++){
-            ans[i]=-1;
-            for(int j=1;j<n;j++){
-                int idx=(i+j)%n;
-                if(nums[idx]>nums[i]){
-                    ans[i]=nums[idx];
-                    break;
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = -1;  // default value
+            // check next n-1 elements circularly
+            for (int j = 1; j < n; j++) {
+                int nextIndex = (i + j) % n; // circular move
+                if (nums[nextIndex] > nums[i]) {
+                    ans[i] = nums[nextIndex];
+                    break; // stop at first greater
                 }
             }
         }
+
         return ans;
     }
 }
